62d65beb5c81a6abeacb865152fdf170cfd4138b
axeanya <ms.anna.aks@gmail.com>
2026-04-17 13:38:34 +0300

    Add files via upload
    Hierarchy file updated. 
    Refactored sql for better readability.

--- Hierarhy.sql
+++ Hierarhy.sql
@@ -1,116 +1,147 @@
 /*
-first create view and Function, then run the anonymous block
+ Firstly create view and Function, then run the anonymous block.
+ You can't run the whole file, syntax error: Create View or Create Function should be the only one in the package. 
+ But you can run the file step by step
+ */
+
+-- alter table bom 
+--add produced_material_quantity_upd float,
+--component_material_quantity_upd FLOAT;
+
+--UPDATE bom
+--    SET 
+--    produced_material_quantity_upd = 
+--        TRY_PARSE(
+--            produced_material_quantity AS DECIMAL(18, 2) USING 'en-US'
+--        ) ,
+--    component_material_quantity_upd = 
+--        TRY_PARSE(
+--            component_material_quantity AS DECIMAL(18, 2) USING 'en-US'
+--        );
+
+
+-- step 1
+CREATE OR ALTER VIEW v_bomGroupBy AS
+SELECT
+    produced_material,
+    produced_material_production_type,
+    produced_material_release_type,
+    sum(produced_material_quantity_upd) material_cnt,
+    component_material,
+    component_material_production_type,
+    component_material_release_type,
+    sum(component_material_quantity_upd) component_cnt,
+    plant_id,
+    year
+FROM
+    bom
+GROUP BY
+    year,
+    produced_material,
+    produced_material_production_type,
+    produced_material_release_type,
+    component_material,
+    component_material_production_type,
+    component_material_release_type,
+    plant_id;
+
+-- step 2 
+CREATE OR ALTER FUNCTION dbo.fn_GetBomHierarhy (
+    @Material int,
+    @PlantID varchar(10),
+    @Year int
+) RETURNS TABLE AS RETURN (
+    -- create Recursive CTE
+    WITH BomHierarhy AS (
+        SELECT
+            plant_id AS plant,
+            produced_material AS fin_material_id,
+            produced_material_release_type AS fin_material_release_type,
+            produced_material_production_type AS fin_material_production_type,
+            material_cnt AS fin_production_quantity,
+            produced_material AS prod_material_id,
+            produced_material_release_type AS prod_material_release_type,
+            produced_material_production_type AS prod_material_production_type,
+            material_cnt AS prod_material_production_quantity,
+            component_material AS component_id,
+            component_material_release_type AS component_material_release_type,
+            component_material_production_type AS component_material_production_type,
+            component_cnt AS component_consumption_quantity,
+            1 AS LEVEL,
+            year
+        FROM
+            v_bomGroupBy
+        WHERE
+            produced_material_release_type = 'FIN'
+            --AND plant_id = @PlantID
+            AND year = @Year
+            --AND produced_material = @Material
+        UNION
+        ALL
+        SELECT
+            t.plant_id AS plant,
+            be.fin_material_id,
+            be.fin_material_release_type,
+            be.fin_material_production_type,
+            be.fin_production_quantity,
+            t.produced_material AS prod_material_id,
+            t.produced_material_release_type AS prod_material_release_type,
+            t.produced_material_production_type AS prod_material_production_type,
+            t.material_cnt AS prod_material_production_quantity,
+            t.component_material AS component_id,
+            t.component_material_release_type AS component_material_release_type,
+            t.component_material_production_type AS component_material_production_type,
+            t.component_cnt AS component_consumption_quantity,
+            be.level + 1,
+            t.year
+        FROM
+            v_bomGroupBy t
+            INNER JOIN BomHierarhy be ON t.produced_material = be.component_id
+            AND t.plant_id = be.plant
+            AND t.year = be.year
+    )
+    SELECT
+        *
+    FROM
+        BomHierarhy b
+    WHERE
+        LEVEL > 1
+);
+/*
+possible indexes for improving performance
+1) compose index for bom(YEAR,produced_material_release_type) for where clause
+2) index for join 
 */
-declare @Material int = 10000
-    ,@PlantID varchar(10) = 'RLT_10'
-    ,@Year int = 2024
 
-    ,@finMaterial int
-    ,@finRelease_type varchar(10)
-    ,@finProd_type int
-    ,@finMaterialCount int;
-
-begin
-    drop table if exists #tempRes;
-
-    SELECT * into #tempRes  
-    FROM dbo.fn_GetBomHierarhy(@Material,@PlantID, @Year); 
-
-    Select @finMaterial = material
-    , @finRelease_type = release_type
-    , @finProd_type =  prod_type 
-    , @finMaterialCount = material_cnt
-    from #tempRes where material = @Material
-
-    select 
-    plant
-    , @finMaterial fin_material_id
-    , @finRelease_type fin_material_release_type
-    , @finProd_type fin_material_production_type
-    , @finMaterialCount fin_production_quantity
-    , material --prod_material_id
-    , release_type --prod_material_release_type
-    , prod_type --prod_material_production_type
-    , material_cnt --prod_material_production_quantity
-    , component --component_id
-    , comp_release_type --component_material_release_type
-    , comp_prod_type --component_material_production_type
-    , component_cnt --Component_consumption_quantity
-    , year
-    from #tempRes
-    where level<>1
-    order by level,comp_prod_type desc;
-end;
-
---create or alter view v_bomGroupBy
---as
---select 
---produced_material
---,produced_material_production_type
---,produced_material_release_type
---,sum(TRY_PARSE(produced_material_quantity AS DECIMAL(18, 2) USING 'en-US')) material_cnt	
---,component_material
---,component_material_production_type
---,component_material_release_type
---,sum(TRY_PARSE(component_material_quantity AS DECIMAL(18, 2) USING 'en-US')) component_cnt
---,plant_id, year from bom
---group by year, produced_material,produced_material_production_type,
---produced_material_release_type
---,component_material,component_material_production_type,component_material_release_type
---,plant_id;
+-- step 3 FINAL
+declare 
+@Material int = 10000,
+@PlantID varchar(10) = 'RLT_10',
+@Year int = 2024 
 
 
---CREATE OR ALTER FUNCTION dbo.fn_GetBomHierarhy (
---    @Material int, 
---    @PlantID varchar(10),
---    @Year int
---)
---RETURNS TABLE 
---AS
---RETURN 
---(
---    -- create Recursive CTE
---    WITH BomHierarhy AS (
---    -- 
---    SELECT 
---        plant_id as plant,
---        year,
---        produced_material as material,
---        produced_material_release_type as release_type,
---        produced_material_production_type as prod_type,
---        material_cnt,
---        component_material as component,
---        component_material_release_type as comp_release_type,
---        component_material_production_type as comp_prod_type,
---        component_cnt,
---        1 AS Level
---    FROM v_bomGroupBy
---    WHERE produced_material_release_type = 'FIN'
---    and plant_id = @PlantID
---    and year = @Year
---    and produced_material = @Material
+SET STATISTICS TIME ON;
+GO
 
---    UNION ALL
+BEGIN
+SELECT
+    *
+FROM
+    fn_GetBomHierarhy(10000, 'RLT_10', 2024);
 
---    -- 
---    SELECT 
---        t.plant_id as plant,
---        t.year,
---        t.produced_material as material,
---        t.produced_material_release_type as release_type,
---        t.produced_material_production_type as prod_type,
---        t.material_cnt, 
---        t.component_material as component,
---        t.component_material_release_type as comp_release_type,
---        t.component_material_production_type as comp_prod_type,
---        t.component_cnt,
---        be.Level + 1
---    FROM v_bomGroupBy t
---    INNER JOIN BomHierarhy be ON t.produced_material = be.component
---        AND t.plant_id = be.plant
---        and t.year = be.year
---)
---SELECT *  
---FROM BomHierarhy b
---)
+END;
 
+GO
+SET STATISTICS TIME OFF;
+
+/*
+   SQL Server Execution Times:
+   CPU time = 16 ms,  elapsed time = 50 ms.
+
+   ->Increase number of rows from 1320 to 133,320
+   SQL Server Execution Times:
+   CPU time = 719 ms,  elapsed time = 832 ms.
+
+   ->Adding indexes on join fields component_material, produced_material doesn't improve performance
+   Because View uses Group by
+*/
